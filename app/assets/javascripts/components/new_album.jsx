d98b72318baa07fb49f3840667f8867385022e26
rails <rails@rails.com>
2016-04-29 00:37:45 +0530

    display image

--- app/assets/javascripts/components/new_album.jsx
+++ app/assets/javascripts/components/new_album.jsx
@@ -1,6 +1,6 @@
 var NewAlbum = React.createClass({
   getInitialState: function() {
-    return {title: '', description: '', avatar: null, errors: {}};
+    return {title: '', description: '', avatar: null, preview_url: null, errors: {}};
   },
 
   handleSubmit: function(e){
@@ -44,6 +44,7 @@ var NewAlbum = React.createClass({
     reader.onload = function(upload) {
       self.setState({
         avatar: upload.target.result,
+        preview_url: reader.result
       });
     }
 
@@ -54,13 +55,38 @@ var NewAlbum = React.createClass({
     return(
       <div>
         <div>
-          <h3>TODO</h3>
-          <form ref='album_form' onSubmit={this.handleSubmit} encType="multipart/form-data">
-            <input type="text" value={this.state.title} name='title' onChange={this.handleChange} />
-            <textarea value={this.state.description} name='description' onChange={this.handleChange} />
-            <input type="file" onChange={this.handleFile} name='avatar' />
-            <button>Submit</button>
+          <h3>Create Album</h3>
+          <form className='form-horizontal' ref='album_form' onSubmit={this.handleSubmit} encType="multipart/form-data">
+            <div className="form-group">
+              <label htmlFor="title" className="control-label col-sm-2">Title:</label>
+              <div className="col-sm-10">
+                <input type="text" value={this.state.title} name='title' onChange={this.handleChange} placeholder="Enter title" className="form-control" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description" className="control-label col-sm-2">Description:</label>
+              <div className="col-sm-10">
+                <textarea value={this.state.description} name='description' onChange={this.handleChange} className="form-control" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-2"></div>
+              <div className="col-sm-10">
+                <input type="file" onChange={this.handleFile} name='avatar'/>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-2"></div>
+              <div className="col-sm-10">
+                <button className='btn btn-primary'>Submit</button>
+              </div>
+            </div>
+            
           </form>
+        </div>
+        <div className='col-md-2'></div>
+        <div className='col-md-3'>
+          {(this.state.preview_url != null) ? <img class='preview_al_image' src={this.state.preview_url} /> : null}
         </div>
       </div>
     )
